b78910090183f41f6aac7144a38c2ec9ea4e059f
Tinonturja <tinonturja@gmail.com>
2025-04-19 13:31:13 +0600

    Creating model.py

---
+++ model.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+
+class TinyVGGModelV0(nn.Module):
+    def __init__(self,
+                 input_shape:int,
+                 hidden_units: int,
+                 output_shape: int):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool1d(2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units,
+                      out_features=output_shape)
+        )
+
+
+    def forward(self,x):
